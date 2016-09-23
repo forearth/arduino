900a257b7380d6c3eb9cad843caa6e1b3618677e
Sangyong Lee <forearth@hanmail.net>
2016-09-23 13:28:57 +0900

    RGB Led Example
    Annode RGB LED

---
+++ RGBLed/RGBLed.ino
@@ -0,0 +1,27 @@
+int redPin = 9;
+int greenPin = 10;
+int bluePin = 11;
+ 
+void setup()
+{
+ pinMode(redPin, OUTPUT);
+ pinMode(greenPin, OUTPUT);
+ pinMode(bluePin, OUTPUT); 
+}
+ 
+void loop()
+{
+  setColor(0, 0, 255); // red
+  delay(2000);
+  setColor(0, 255, 0); //green
+  delay(2000);
+  setColor(255, 0, 0); // blue.
+  delay(2000);
+}
+ 
+void setColor(int red, int green, int blue)
+{
+  analogWrite(redPin, 255-red);
+  analogWrite(greenPin, 255-green);
+  analogWrite(bluePin, 255-blue); 
+}
