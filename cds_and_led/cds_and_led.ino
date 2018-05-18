ae6c7621a3e07a4e591094cef5f5c239342388cc
Sangyong Lee <forearth@hanmail.net>
2018-05-19 00:49:34 +0900

    upload cds&led exam

---
+++ cds_and_led/cds_and_led.ino
@@ -0,0 +1,22 @@
+#define LIGHT_PIN A0
+#define LED 3
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);  
+}
+void loop()
+{
+  int val = 0;
+  val = analogRead(LIGHT_PIN); 
+  Serial.println(val);
+  delay(1000);
+
+  if(val>400){
+    digitalWrite(LED, LOW);
+  }else{
+    digitalWrite(LED, HIGH);
+  }  
+}
+
