3fa354337c653e4905a464daf7c9a416813f4895
Sangyong Lee <forearth@hanmail.net>
2018-05-30 09:58:59 +0900

    serial 통신 예제

---
+++ serial_sample/serial_sample.ino
@@ -0,0 +1,14 @@
+//시작할 때 한번실행. 초기화 파트
+void setup() {
+  Serial.begin(9600);
+}
+
+//반복실행 파트
+void loop() {
+  Serial.println("안녕하세요. 시작합니다.");
+  for(int i=0; i<=3; i++){
+    Serial.print("hello");
+    Serial.println(i);
+    delay(1000);    
+  }  
+}
